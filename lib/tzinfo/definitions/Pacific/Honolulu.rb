5f527c362193416eec5456a93a5023deb2d3276c
Ryan Rolland <ryan.rolland@ficoh.com>
2017-04-13 09:41:42 -1000

    different change approach

--- lib/tzinfo/definitions/Pacific/Honolulu.rb
+++ lib/tzinfo/definitions/Pacific/Honolulu.rb
@@ -6,9 +6,17 @@ module TZInfo
       module Honolulu
         include TimezoneDefinition
         timezone 'Pacific/Honolulu' do |tz|
+          tz.offset :o0, -36000, 0, :LMT
+          tz.offset :o1, -36000, 0, :HST
+          tz.offset :o2, -36000, 0, :HDT
           tz.offset :o3, -36000, 0, :HST
-          #year, month, offset_id, numerator_or_time, denominator = nil
-          tz.transition 1753, 6, :o3, 116752561, 48
+
+          tz.transition 1896, 1, :o1, 104266329343, 43200
+          tz.transition 1933, 4, :o2, 116505265, 48
+          tz.transition 1933, 5, :o1, 116506291, 48
+          tz.transition 1942, 2, :o2, 116659201, 48
+          tz.transition 1945, 9, :o1, 116722991, 48
+          tz.transition 1947, 6, :o3, 116752561, 48
         end
       end
     end
